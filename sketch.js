68bd82d95886009cad7d1aae60369a8f423b6d8f
0702317 <0702317@spsd.sk.ca>
2025-11-18 08:42:51 -0600

    added comment header

--- sketch.js
+++ sketch.js
@@ -1,10 +1,6 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+// Rubik's Cube Simlator/Solve Timer
+// Graham Lindsay
+// January 19th, 2025
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
