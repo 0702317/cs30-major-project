1c185b4042fa73b0323afd2e5c778cfdfbe71f48
0702317 <0702317@spsd.sk.ca>
2025-11-19 09:51:36 -0600

    added WEBGL and displayed a cube

--- sketch.js
+++ sketch.js
@@ -2,10 +2,36 @@
 // Graham Lindsay
 // January 19th, 2025
 
+// I am going to have to use squares for each side of the piece because they are different colours.
+let cube;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  cube = new Piece(0, 0, "corner", color(255, 255, 0));
+
+  debugMode();
 }
 
 function draw() {
   background(220);
+  cube.display();
+  orbitControl();
+}
+
+class Piece {
+  constructor(x, y, type, color) {
+    this.x = x;
+    this.y = y;
+    this.type = type;
+    this.color = color;
+  }
+  
+  display() {
+    fill(this.color);
+    box(100, 100, 100);
+  }
+
+  move() {
+
+  }
 }
