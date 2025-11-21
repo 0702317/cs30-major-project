36b19dd894eb68b0960a0ed137b20fb4738f805e
0702317 <0702317@spsd.sk.ca>
2025-11-21 09:54:06 -0600

    created a piece and started working on displaying multiple

--- sketch.js
+++ sketch.js
@@ -2,59 +2,96 @@
 // Graham Lindsay
 // January 19th, 2025
 
-// I am going to have to use squares for each side of the piece because they are different colours.
+
 let cube = [];
 let piece;
+let cubeSize = 3;
+let pieceSize = 100;
+let moves = ["U", "D", "F", "B", "L", "R"];
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  piece = new ConstantSourceNode(0, 0, "corner", color(255, 255, 0));
 
+  generateCube();
   debugMode();
   angleMode(DEGREES);
 }
 
 function draw() {
   background(220);
-  piece.display();
-  orbitControl();
-  if (keyIsPressed) {
-    piece.move();
-    piece.display();
+  for (let somePiece of cube) {
+    somePiece.display();
   }
+  orbitControl();
 }
-
-
 
 class Piece {
-  constructor(x, y, type, color) {
-    this.x = x;
-    this.y = y;
-    this.type = type;
-    this.color = color;
+  constructor(x, y, z) {
+    this.x = x * pieceSize;
+    this.y = y * pieceSize;
+    this.z = z * pieceSize;
   }
   
-  display() {
-    fill(this.color);
-    box(100, 100, 100);
-  }
-  
-  move() {
-    
-    translate(100, 0, 0);
+  display() { // creates a piece made up of 6 different boxes of different colours that are offset to make each side of the piece a different colour.
+    strokeWeight(4);
+    push();
+    translate(0, -3, 0);
+    fill("white");
+    box(pieceSize);
+    pop();
+    push();
+    translate(0, 3, 0);
+    fill("yellow");
+    box(pieceSize);
+    pop();
+    push();
+    translate(3, 0, 0);
+    fill("orange");
+    box(pieceSize);
+    pop();
+    push();
+    translate(-3, 0, 0);
+    fill("red");
+    box(pieceSize);
+    pop();
+    push();
+    translate(0, 0, 3);
+    fill("green");
+    box(pieceSize);
+    pop();
+    push();
+    translate(0, 0, -3);
+    fill("blue");
+    box(pieceSize);
+    pop();
+    translate(this.x, this.y, this.z);
   }
 }
 
-class Edge extends Piece {
-  constructor(x, y, color) {
-    super(x, y, color);
-    this.type = "edge";
-  }
-}
+// class Edge extends Piece {
+//   constructor(x, y, color) {
+//     super(x, y, color);
+//     this.type = "edge";
+//   }
+// }
 
-class Corner extends Piece {
-  constructor(x, y, color) {
-    super(x, y, color);
-    this.type = "corner";
-  }
+// class Corner extends Piece {
+//   constructor(x, y, color) {
+//     super(x, y, color);
+//     this.type = "corner";
+//   }
+// }
+
+function generateCube() {
+  // for (let z = 0; z < 3; z++) {
+  //   for (let y = 0; y < 3; y++) {
+  //     for (let x = 0; x < 3; x++) {
+  //       piece = new Piece(x, y, z);
+  //       cube.push(piece);
+  //     }
+  //   }
+  // }
+
+  piece = new Piece(0, 0, 0);
+  cube.push(piece);
 }
