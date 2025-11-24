9e8cf6b732942631fc1ec89538910828066ab539
0702317 <0702317@spsd.sk.ca>
2025-11-24 09:51:53 -0600

    working on generating a cube out of the pieces

--- sketch.js
+++ sketch.js
@@ -2,7 +2,6 @@
 // Graham Lindsay
 // January 19th, 2025
 
-
 let cube = [];
 let piece;
 let cubeSize = 3;
@@ -13,23 +12,24 @@ function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 
   generateCube();
-  debugMode();
+  // debugMode();
   angleMode(DEGREES);
 }
 
 function draw() {
-  background(220);
+  background(100);
   for (let somePiece of cube) {
     somePiece.display();
+    // translate(somePiece.x, somePiece.y, somePiece.z);
   }
   orbitControl();
 }
 
 class Piece {
   constructor(x, y, z) {
-    this.x = x * pieceSize;
-    this.y = y * pieceSize;
-    this.z = z * pieceSize;
+    this.x = x;
+    this.y = y;
+    this.z = z;
   }
   
   display() { // creates a piece made up of 6 different boxes of different colours that are offset to make each side of the piece a different colour.
@@ -68,30 +68,13 @@ class Piece {
   }
 }
 
-// class Edge extends Piece {
-//   constructor(x, y, color) {
-//     super(x, y, color);
-//     this.type = "edge";
-//   }
-// }
-
-// class Corner extends Piece {
-//   constructor(x, y, color) {
-//     super(x, y, color);
-//     this.type = "corner";
-//   }
-// }
-
 function generateCube() {
-  // for (let z = 0; z < 3; z++) {
-  //   for (let y = 0; y < 3; y++) {
-  //     for (let x = 0; x < 3; x++) {
-  //       piece = new Piece(x, y, z);
-  //       cube.push(piece);
-  //     }
-  //   }
-  // }
-
-  piece = new Piece(0, 0, 0);
-  cube.push(piece);
+  for (let z = 0; z < cubeSize; z++) {
+    for (let y = 0; y < cubeSize; y++) {
+      for (let x = 0; x < cubeSize; x++) {
+        let piece = new Piece(x +100, y+100, z+100);
+        cube.push(piece);
+      }
+    }
+  }
 }
