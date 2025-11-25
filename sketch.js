6e6d37e1d7c6591e07e83a61a20dd9cb13912abc
0702317 <0702317@spsd.sk.ca>
2025-11-25 09:47:50 -0600

    displayed a cube
    created a cube made of the pieces and centered it in the camera

--- sketch.js
+++ sketch.js
@@ -5,22 +5,27 @@
 let cube = [];
 let piece;
 let cubeSize = 3;
-let pieceSize = 100;
+let pieceSize = 300/cubeSize;
 let moves = ["U", "D", "F", "B", "L", "R"];
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-
+  strokeWeight(4);
   generateCube();
   // debugMode();
   angleMode(DEGREES);
+
+  // back face culling.
+  drawingContext.enable(drawingContext.CULL_FACE);
+  drawingContext.cullFace(drawingContext.FRONT);
+
+  camera(500, -500, 500, pieceSize, pieceSize, pieceSize, 0, 1, 0);
 }
 
 function draw() {
   background(100);
   for (let somePiece of cube) {
     somePiece.display();
-    // translate(somePiece.x, somePiece.y, somePiece.z);
   }
   orbitControl();
 }
@@ -31,40 +36,16 @@ class Piece {
     this.y = y;
     this.z = z;
   }
-  
+
   display() { // creates a piece made up of 6 different boxes of different colours that are offset to make each side of the piece a different colour.
-    strokeWeight(4);
-    push();
-    translate(0, -3, 0);
-    fill("white");
-    box(pieceSize);
-    pop();
-    push();
-    translate(0, 3, 0);
-    fill("yellow");
-    box(pieceSize);
-    pop();
-    push();
-    translate(3, 0, 0);
-    fill("orange");
-    box(pieceSize);
-    pop();
-    push();
-    translate(-3, 0, 0);
-    fill("red");
-    box(pieceSize);
-    pop();
-    push();
-    translate(0, 0, 3);
-    fill("green");
-    box(pieceSize);
-    pop();
-    push();
-    translate(0, 0, -3);
-    fill("blue");
-    box(pieceSize);
-    pop();
+    resetMatrix();
     translate(this.x, this.y, this.z);
+    piece = buildGeometry(createPiece);
+    model(piece);
+  }
+
+  turn() {
+
   }
 }
 
@@ -72,9 +53,41 @@ function generateCube() {
   for (let z = 0; z < cubeSize; z++) {
     for (let y = 0; y < cubeSize; y++) {
       for (let x = 0; x < cubeSize; x++) {
-        let piece = new Piece(x +100, y+100, z+100);
-        cube.push(piece);
+        cube.push(new Piece(x * pieceSize, y * pieceSize, z * pieceSize));
       }
     }
   }
+}
+
+function createPiece() { // design for a piece
+  push();
+  translate(0, -3, 0);
+  fill("white");
+  box(pieceSize);
+  pop();
+  push();
+  translate(0, 3, 0);
+  fill("yellow");
+  box(pieceSize);
+  pop();
+  push();
+  translate(3, 0, 0);
+  fill("orange");
+  box(pieceSize);
+  pop();
+  push();
+  translate(-3, 0, 0);
+  fill("red");
+  box(pieceSize);
+  pop();
+  push();
+  translate(0, 0, 3);
+  fill("green");
+  box(pieceSize);
+  pop();
+  push();
+  translate(0, 0, -3);
+  fill("blue");
+  box(pieceSize);
+  pop();
 }
